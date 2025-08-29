9c4d87c2b19d28b83552d9c894a0f97b83ab73c1
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-08-29 06:23:27 +0000

    doing exercise

--- Udemy_Boothcamp/2.Basic_CRUD/query.sql
+++ Udemy_Boothcamp/2.Basic_CRUD/query.sql
@@ -5,3 +5,11 @@ SELECT * from cats WHERE name = "EGG";
 -- Nội dung sau WHERE chạy trước
 SELECT name FROM cats WHERE age = 4;
 
+-- Rapid Fire Exercise
+select cat_id, age from cats where cat_id = age;
+select name, age from cats where breed = "Tabby";
+select name, breed from cats;
+select cat_id from cats;
+
+
+

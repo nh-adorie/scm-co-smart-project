925b3ddd27cbaab528cd06b7fef4357ab11babb5
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-09-09 04:55:59 +0000

    string function: replace, reverse

--- Udemy_Boothcamp/3.String_Function/query.sql
+++ Udemy_Boothcamp/3.String_Function/query.sql
@@ -1,23 +1,22 @@
--- Khi chạy SELECT 'adorie'; hay SELECT 1+4;
--- Sẽ hiện ra kết quả là 'adorie' hay 5
--- Có thể hiểu SELECT giống như "Hãy chọn những cái này cho tôi xem"
-
 SELECT 
     CONCAT(author_fname,' ',author_lname) AS full_name 
 FROM books;
--- Chỉ tạo ra cột mới cho mình xem chứ không làm thay đổi dtb gốc (vì nó nằm trong câu SELECT - chỉ truy vấn dữ liệu thui)
+
 
 SELECT 
     CONCAT_WS(' ', author_fname,author_lname) AS full_name 
 FROM books;
 
+
 SELECT 
     SUBSTRING(title,1,15) AS short_title  
 FROM books;
 
+
 SELECT 
     SUBSTR(title,1,15) AS short_title  
 FROM books;
+
 
 SELECT 
     CONCAT(
@@ -25,6 +24,7 @@ SELECT
         '...'
     ) AS short_title
 FROM books;
+
 
 SELECT 
     CONCAT_WS(
@@ -34,4 +34,14 @@ SELECT
     ) AS authors_initials
 FROM books;
 
--- Có thể cài thêm extention SQL Formatter trong VS Code để nó tự format SQL Code cho đẹp
+
+SELECT
+    REPLACE(
+        title,
+        ' ',
+        '_'
+    )
+FROM books;
+
+
+SELECT REVERSE(author_fname) FROM books;

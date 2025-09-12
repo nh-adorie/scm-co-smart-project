3401e0f83f56836929b097b2f235c87e6bed363b
Adorie Nguyen <thuongnguyen271000@gmail.com>
2025-09-12 09:03:13 +0000

    window function

--- Udemy_Boothcamp/10.WindowFunction/queries.sql
+++ Udemy_Boothcamp/10.WindowFunction/queries.sql
@@ -5,3 +5,33 @@
 -- OVER()
 -- The OVER() clause constructs a window.
 -- When it's empty, the window will include all record
+
+SELECT department, AVG(salary) 
+FROM employees
+GROUP BY department;
+
+SELECT AVG(salary) FROM employees;
+
+SELECT AVG(salary) OVER() FROM employees;
+
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    AVG(salary) OVER(),
+    MIN(salary) OVER(),
+    MAX(salary) OVER() 
+FROM employees;
+
+-- PARTITION BY
+-- Inside of the OVER()
+-- use PARTITION BY to form rows into window ~ groups of rows
+
+SELECT 
+    emp_no, 
+    department, 
+    salary, 
+    AVG(salary) OVER(PARTITION BY department),
+    MIN(salary) OVER(PARTITION BY department),
+    MAX(salary) OVER(PARTITION BY department) 
+FROM employees;

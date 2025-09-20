248e31bad10f4eeaaed1fcf77c04d0505604af26
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-09-20 07:53:08 +0000

    build container

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
 echo "Starting setup..."
 
 python3 -m venv /workspaces/venv
 source /workspaces/venv/bin/activate
 
+pip install --upgrade pip
 pip install numpy pandas matplotlib seaborn scikit-learn jupyterlab kaggle sqlalchemy mysql-connector-python
 
 sudo apt-get install -y mysql-server

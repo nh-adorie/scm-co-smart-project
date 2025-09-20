fb5672a58d9504dcf7902b29edd6b562f83309d6
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-09-20 06:44:48 +0000

    setup codespace

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Starting setup..."
+
+python3 -m venv /workspaces/venv
+source /workspaces/venv/bin/activate
+
+pip install numpy pandas matplotlib seaborn scikit-learn jupyterlab kaggle sqlalchemy mysql-connector-python
+
+sudo apt-get install -y mysql-server
+sudo service mysql start
+
+echo "Setup complete!"
